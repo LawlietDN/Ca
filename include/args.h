69188c8838e016b648d8014524ec81e08b21bb3c
LawlietDN <ufcgamer82@gmail.com>
2025-01-19 15:34:39 +0300

    Moved into include directory.

---
+++ include/args.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <boost/program_options.hpp>
+#include <regex>
+namespace args
+{
+    bool check(int argc, char* argv[], uint16_t& port, std::string& origin);
+    bool checkPort(uint16_t const& port, boost::program_options::options_description const& description);
+    bool isValidURL(std::string const& url, boost::program_options::options_description const& description);
+
+};
