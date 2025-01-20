dcc1e1fa4012687b57e4eed57b277b3a5d30aa70
LawlietDN <ufcgamer82@gmail.com>
2025-01-20 20:52:02 +0300

    session.cpp: Cleaned up code.

--- src/Network/Session/session.cpp
+++ src/Network/Session/session.cpp
@@ -1,23 +1,21 @@
 #include "../Include/session.h"
 
-void Session::read()
+void Session::read(std::string const& origin)
 {
     std::shared_ptr<Session> self = shared_from_this();
     _socket.async_read_some(boost::asio::buffer(data, buffer),
-    [self](boost::system::error_code e, size_t bytesRecieved)
+    [self, origin](boost::system::error_code e, size_t bytesRecieved)
     {
         if(e)
         {
             std::cerr << "Error receiving data: " << e.message();
             return;
         }
-        self->handleRequests(bytesRecieved);
-        std::cout << "\nPath:" << self->path << '\n';
-        std::cout << self->incomingData;
-        self->read();
+        self->handleRequests(bytesRecieved, origin);
+        self->read(origin);
     });
 }
-void Session::handleRequests(size_t bytesRecieved)
+void Session::handleRequests(size_t bytesRecieved, std::string const& origin)
 {
         incomingData.append(data.data(), bytesRecieved);
         auto position = incomingData.find("\r\n");
@@ -31,17 +29,19 @@ void Session::handleRequests(size_t bytesRecieved)
         std::string method;
         iss >> method >> path;
         }
-        
+
         if(path == "/")
         {
            std::cerr << "No path has been specified\n";
+           _socket.close();
            return;
         }
+        Cache Cache(origin, path);
 }
 
 
 
-void Session::start()
+void Session::start(std::string const& origin)
 {
-    read();
+    read(origin);
 }
