2d0ed049bc5f8b70d58e8c501c3dbb4a055392d6
LawlietDN <ufcgamer82@gmail.com>
2025-01-20 13:11:42 +0300

    args.cpp; Moved into /CLI directory

--- src/CLI/args.cpp
+++ src/CLI/args.cpp
@@ -1,11 +1,12 @@
-#include "args.h"
+#include "../Include/args.h"
 
 namespace args
 {
     bool isValidURL(std::string const& url, boost::program_options::options_description const& description)
     {
-    const std::regex url_regex(R"((https?)://[^\s/$.?#].[^\s]*)");
+     const std::regex url_regex(R"((https?)://[^\s/$.?#].[^\s]*)");
      if(std::regex_match(url, url_regex)) return true;
+
      std::cerr << "Invalid URL\n";
      std::cerr << description;
      return false;
@@ -17,7 +18,7 @@ namespace args
         if(port < 1024 || port > 65535)
         {
             std::cerr << "Error: Port numebr must be between 1024 and 65535\n";
-            std::cout << description;
+            std::cerr << description;
             return false;
         }
         return true;
@@ -28,21 +29,26 @@ namespace args
     {
         boost::program_options::options_description description("Usage"); 
          description.add_options()
-        ("port, p", boost::program_options::value<uint16_t>(&port)->required(), "The port number on which the caching proxy server will run(e.g., 12345).")
-        ("origin, p", boost::program_options::value<std::string>(&origin)->required(), "The URL of the server to which the requests will be forwarded.(e.g., http://dummyjson.com).");
+        ("port, --p", boost::program_options::value<uint16_t>(&port)->required(), "The port number on which the caching proxy server will run(e.g., 12345).")
+        ("origin, p", boost::program_options::value<std::string>(&origin)->required(), "The URL of the server to which the requests will be forwarded.(e.g., http://dummyjson.com).")
+        ("clear-cache", "To clear the cached responses from the server");
         boost::program_options::variables_map variablesMap;
 
     try
     {
 
         boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), variablesMap);
-        
+        // if(variablesMap.count("clear-cache"))
+        // {
+        //     std::cout << "Clearing the cache JSON...\n"; //Logic being implemented soon.
+        //     return true;
+        // }
         if (!variablesMap.count("port"))
         {
              /** Boost's notiy function validates all required arguments but doesn't enforce an order by default.
              *   Hence why the program kept throwing an error indicating that --origin is required when no
              *   arguments are passed, when in fact --port is required if we follow the required arguments order.
-             *   Hence why we need to do a manual check for the port argument to avoid confusion*/
+             *   Hence why we need to do a manual check for the port argument to avoid confusion. */
             std::cerr << "Error: --port option is required.\n";
             std::cerr << description;   
             return false;
