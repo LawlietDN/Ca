9c1d5c006bb987977ac9942556d94f99edc531b4
LawlietDN <ufcgamer82@gmail.com>
2025-01-20 20:48:51 +0300

    server: A getter method for _origin.

--- Include/server.h
+++ Include/server.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "session.h"
 
 class Server
@@ -5,15 +6,17 @@ class Server
 private:
     boost::asio::io_context& io;
     boost::asio::ip::tcp::acceptor acceptor;
+    std::string _origin;
     void accept();
 
 public:
     explicit Server(boost::asio::io_context& ioContext, uint16_t const& port, std::string const& origin, std::string const& address)
     :   io(ioContext),
+        _origin(origin),
         acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(address), port))
         {
             std::cout << "Listening on: " << address << ":" << port << '\n';
             accept();
         }
-
+    std::string getOrigin();
 };
