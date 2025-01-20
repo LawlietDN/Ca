95818d78fde39287e8f5376f16e3909b07c811fe
LawlietDN <ufcgamer82@gmail.com>
2025-01-20 13:17:12 +0300

    main.cpp; Created an io instance which is passed to the constructor of Server class and other additional variables.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
-#include "args.h"
-
-
+#include "../Include/args.h"
+#include "../Include/server.h"
 
 int main(int argc, char *argv[])
 {
     uint16_t port = 0;
     std::string origin;
+    std::string address = "127.0.0.1";
     if(!args::check(argc, argv, port, origin)) return 1;
     
-    std::cout << "Port: " << port << "\nOrigin: " << origin;
-    
-    
+    std::cout << "Port: " << port << "\nOrigin: " << origin << '\n';
+    try
+    {
+        boost::asio::io_context io;
+        Server Server(io, port, origin, address);
+        io.run();
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what();
+    }
     return 0;
    
 }
