4e5cce6e868fbbd0b0cbecf1bc3bf4684c57f086
LawlietDN <ufcgamer82@gmail.com>
2025-01-21 23:38:26 +0300

    Server: Changed the unnecessary storage of the io instance as a member and directly passes it to the acceptor.

--- Include/server.h
+++ Include/server.h
@@ -4,15 +4,13 @@
 class Server
 {
 private:
-    boost::asio::io_context& io;
     boost::asio::ip::tcp::acceptor acceptor;
     std::string _origin;
     void accept();
 
 public:
-    explicit Server(boost::asio::io_context& ioContext, uint16_t const& port, std::string const& origin, std::string const& address)
-    :   io(ioContext),
-        _origin(origin),
+    explicit Server(boost::asio::io_context& io, uint16_t const& port, std::string const& origin, std::string const& address)
+    :   _origin(origin),
         acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(address), port))
         {
             std::cout << "Listening on: " << address << ":" << port << '\n';
