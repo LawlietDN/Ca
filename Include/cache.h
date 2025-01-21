1add6d95ab030675f1ee3216a9bce0fda8f6f2da
LawlietDN <ufcgamer82@gmail.com>
2025-01-21 23:36:19 +0300

    cache: Declarations.

--- Include/cache.h
+++ Include/cache.h
@@ -1,16 +1,23 @@
 #include <iostream>
-class Cache
+#include <memory>
+#include <boost/asio.hpp>
+class Cache: public std::enable_shared_from_this<Cache>
 {
 private:
-    void forward();
+    boost::asio::ip::tcp::socket _socket;
+    boost::asio::ip::tcp::resolver resolver;
+     boost::asio::streambuf responseBuffer;
     std::string _origin;
     std::string _path;
-
+    void forward();
+    void connect();
+    void readResponse();
 public:
-    explicit Cache(std::string const& origin, std::string const& path)
-    :   _origin(origin),
-        _path(path)
-        {
-            forward();
-        }
+    explicit Cache( boost::asio::io_context& io, std::string const& origin, std::string const& path)
+    :   _socket(io),
+        resolver(io),
+        _origin(origin),
+        _path(path) {}
+    
+    void start();
 };
