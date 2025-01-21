bda219005ee02f3f511b65542f3f72cf4d7dc442
LawlietDN <ufcgamer82@gmail.com>
2025-01-21 23:45:44 +0300

    Session: Code improvements and removed some redundant lines of code.

--- src/Network/Session/session.cpp
+++ src/Network/Session/session.cpp
@@ -1,9 +1,9 @@
-#include "../Include/session.h"
+#include "session.h"
 
 void Session::read(std::string const& origin)
 {
-    std::shared_ptr<Session> self = shared_from_this();
-    _socket.async_read_some(boost::asio::buffer(data, buffer),
+    auto self = shared_from_this();
+    _socket.async_read_some(boost::asio::buffer(data),
     [self, origin](boost::system::error_code e, size_t bytesRecieved)
     {
         if(e)
@@ -11,32 +11,26 @@ void Session::read(std::string const& origin)
             std::cerr << "Error receiving data: " << e.message();
             return;
         }
-        self->handleRequests(bytesRecieved, origin);
+        if(!self->isValidRequests(bytesRecieved)) return;
+        std::make_shared<Cache>(static_cast<boost::asio::io_context&>(self->_socket.get_executor().context()), origin, self->path)->start();
         self->read(origin);
     });
 }
-void Session::handleRequests(size_t bytesRecieved, std::string const& origin)
+bool Session::isValidRequests(size_t bytesRecieved)
 {
-        incomingData.append(data.data(), bytesRecieved);
-        auto position = incomingData.find("\r\n");
 
-        if(position != std::string::npos)
-        {
-        std::string requestLine = incomingData.substr(0, position);
-        incomingData.erase(0, position + 2);
+    std::string requestLine(data.data(), bytesRecieved);
 
-        std::istringstream iss(requestLine);
-        std::string method;
-        iss >> method >> path;
-        }
+    std::istringstream iss(requestLine);
+    std::string method;
+    iss >> method >> path;
 
-        if(path == "/")
-        {
-           std::cerr << "No path has been specified\n";
-           _socket.close();
-           return;
-        }
-        Cache Cache(origin, path);
+    if (path == "/")
+    {
+        std::cerr << "No path has been specified\n";
+        return false;
+    }
+    return true;
 }
 
 
