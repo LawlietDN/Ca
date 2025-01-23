fe45048f5e11c4d70a927a4266c883b8fc63946f
LawlietDN <ufcgamer82@gmail.com>
2025-01-23 11:05:49 +0300

    Cache: Code improvements, and stuck on a particular problem.

--- src/Network/Session/session.cpp
+++ src/Network/Session/session.cpp
@@ -2,6 +2,8 @@
 
 void Session::read(std::string const& origin)
 {
+    std::cout << "ddddk\n";
+
     auto self = shared_from_this();
     _socket.async_read_some(boost::asio::buffer(data),
     [self, origin](boost::system::error_code e, size_t bytesRecieved)
@@ -11,9 +13,10 @@ void Session::read(std::string const& origin)
             std::cerr << "Error receiving data: " << e.message();
             return;
         }
+        std::cout << "ddddk\n";
         if(!self->isValidRequests(bytesRecieved)) return;
+        std::cout << "eee\n";
         std::make_shared<Cache>(static_cast<boost::asio::io_context&>(self->_socket.get_executor().context()), origin, self->path)->start();
-        self->read(origin);
     });
 }
 bool Session::isValidRequests(size_t bytesRecieved)
@@ -24,18 +27,28 @@ bool Session::isValidRequests(size_t bytesRecieved)
     std::istringstream iss(requestLine);
     std::string method;
     iss >> method >> path;
-
-    if (path == "/")
-    {
-        std::cerr << "No path has been specified\n";
-        return false;
-    }
+    
     return true;
 }
 
+void Session::write(std::string const& message)
+{
+    auto self = shared_from_this();
+    boost::asio::async_write(_socket, boost::asio::buffer(message),
+    [self](boost::system::error_code e, size_t transferredBytes)
+    {
+        if(e)
+        {
+            std::cerr << "Error while writing: " << e.message();
+            return;
+        }
 
+    });
+}
 
 void Session::start(std::string const& origin)
 {
-    read(origin);
+        read(origin);
+    
 }
+
