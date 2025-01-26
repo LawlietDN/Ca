d300d44398faa83a0da384818fa32b93be3a58f0
LawlietDN <ufcgamer82@gmail.com>
2025-01-27 00:21:42 +0300

    cache: Function declarations.

--- Include/cache.h
+++ Include/cache.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <memory>
 #include <boost/asio.hpp>
+#include <nlohmann-json.hpp>
+#include <fstream>
+#include <filesystem>
 class Cache: public std::enable_shared_from_this<Cache>
 {
 private:
@@ -12,13 +15,17 @@ private:
     boost::asio::streambuf responseBuffer;
     std::string _origin;
     std::string _path;
+    std::string fileName = "../src/Network/Cache/cache.json";
     std::array<char, 1024> data;
     void write(std::string const& message, boost::asio::ip::tcp::socket& socket);
     void connect();
     void readResponse();
     void trim();
     std::string requestMessage();
-
+    void store(std::string const& identifier, nlohmann::json const& cachedResponse);
+    bool isCached();
+    std::string makeHeaders(size_t messageSize);
+    void changeContentLength(std::string& fullResponse);
 public:
     explicit Cache(boost::asio::ip::tcp::socket& clientSocket, boost::asio::io_context& io, std::string const& origin, std::string const& path)
     :   _clientSocket(clientSocket),
@@ -27,5 +34,5 @@ public:
         _origin(origin),
         _path(path) {}
     
-    bool start();
+    void start();
 };
