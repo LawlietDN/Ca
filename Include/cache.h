a9c43cf7cd7bf5291d15f1ac4de1bb8c28d8ebf5
LawlietDN <ufcgamer82@gmail.com>
2025-01-28 11:33:16 +0300

    Removing header includes to each other to fix  circular dependencies.

--- Include/cache.h
+++ Include/cache.h
@@ -1,22 +1,21 @@
 #pragma once
-#include "session.h"
 #include <iostream>
 #include <memory>
 #include <boost/asio.hpp>
 #include <nlohmann-json.hpp>
 #include <fstream>
 #include <filesystem>
+class Session;
+
 class Cache: public std::enable_shared_from_this<Cache>
 {
 private:
-    boost::asio::ip::tcp::socket& _clientSocket;
+    std::shared_ptr<Session> _session;
     boost::asio::ip::tcp::socket _socket;
     boost::asio::ip::tcp::resolver resolver;
     boost::asio::streambuf responseBuffer;
     std::string _origin;
     std::string _path;
-    std::string fileName = "../src/Network/Cache/cache.json";
-    std::array<char, 1024> data;
     void write(std::string const& message, boost::asio::ip::tcp::socket& socket);
     void connect();
     void readResponse();
@@ -26,13 +25,15 @@ private:
     bool isCached();
     std::string makeHeaders(size_t messageSize);
     void changeContentLength(std::string& fullResponse);
+    void handleResponse(boost::asio::streambuf& responseBuffer);
+
 public:
-    explicit Cache(boost::asio::ip::tcp::socket& clientSocket, boost::asio::io_context& io, std::string const& origin, std::string const& path)
-    :   _clientSocket(clientSocket),
+    explicit Cache(std::shared_ptr<Session> session, boost::asio::io_context& io, std::string const& origin, std::string const& path)
+    :   _session(session),
         _socket(io),
         resolver(io),
         _origin(origin),
         _path(path) {}
-    
-    void start();
+    inline static std::string fileName = "../src/Network/Cache/cache.json";
+    bool start();
 };
--- Include/session.h
+++ Include/session.h
@@ -1,17 +1,19 @@
 #pragma once
-#include "cache.h"
 #include <boost/asio.hpp>
 #include <memory>
 #include <iostream>
 
+class Cache;
+
 class Session: public std::enable_shared_from_this<Session>
 {
+    friend class Cache;
 private:
     boost::asio::ip::tcp::socket _socket;
     std::array<char, 1024> data;
     std::string incomingData;
     std::string path;
-    bool isValidRequests(size_t bytesRecieved);
+    void storePath(size_t bytesRecieved);
 
     void read(std::string const& origin);
     void write(std::string const& message);
