de9a01b60fcf82331b2f114fb8cba43e26b9445c
LawlietDN <ufcgamer82@gmail.com>
2025-01-28 11:50:00 +0300

    Session: Project completion. Fixed a huge error where segmentation fault occurs inside the async_read_some operation where if  inside the callback the function read does not call it self, a segmentation fault will occur because the  Session's reference count drops to zero since there isn't any more pending asynchronous operations and while Cache is initiating   asynchronous operations that  attempt to access the Session's socket after it has been destroyed.

--- src/Network/Session/session.cpp
+++ src/Network/Session/session.cpp
@@ -1,22 +1,20 @@
 #include "session.h"
-#include <nlohmann-json.hpp>
+#include "cache.h"
 void Session::read(std::string const& origin)
 {
     auto self = shared_from_this();
     _socket.async_read_some(boost::asio::buffer(data),
     [self, origin](boost::system::error_code const& e, size_t const& bytesRecieved)
     {
-        if(e)
+        if(e && e != boost::asio::error::eof)
         {
-            std::cerr << "Error receiving data: " << e.message() << '\n';
             return;
         }
-        if(!self->isValidRequests(bytesRecieved)) return;
-        std::make_shared<Cache>(self->_socket, static_cast<boost::asio::io_context&>(self->_socket.get_executor().context()), origin, self->path)->start();
-        self->read(origin);
+        self->storePath(bytesRecieved);
+        std::make_shared<Cache>(self, static_cast<boost::asio::io_context&>(self->_socket.get_executor().context()), origin, self->path)->start();
     });
 }
-bool Session::isValidRequests(size_t bytesRecieved)
+void Session::storePath(size_t bytesRecieved)
 {
 
     std::string requestLine(data.data(), bytesRecieved);
@@ -25,7 +23,6 @@ bool Session::isValidRequests(size_t bytesRecieved)
     std::string method;
     iss >> method >> path;
     
-    return true;
 }
 
 void Session::write(std::string const& message)
@@ -36,7 +33,6 @@ void Session::write(std::string const& message)
     {
         if(e && e != boost::asio::error::eof)
         {
-            std::cerr << "Error while writing: " << e.message();
             return;
         }
 
